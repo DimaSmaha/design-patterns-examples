34f5de839b02000d2ffe8921d9fae5dd150b677a
Dima Smaha <fannifty@gmail.com>
2024-03-12 12:06:58 +0200

    some upd

--- singleton/class.ts
+++ singleton/class.ts
@@ -1,3 +1,11 @@
+/**
+ * We use singleton pattern in order to get only the one
+ * entity of some object of state
+ * Generally its used for the driver
+ * When we initialize the driver in one place
+ * And then we can use this drivers all over the test files
+ */
+
 export class RandomNumber {
   private static randomNumber: any;
   private constructor() {}
