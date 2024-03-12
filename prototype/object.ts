02d8e003cccd5047dc31fa2d0a79456568679195
Dima Smaha <fannifty@gmail.com>
2024-03-12 12:30:55 +0200

    normal proto

--- prototype/object.ts
+++ prototype/object.ts
@@ -34,31 +34,41 @@ export class UserData implements IUserData {
   favGame: string;
   friends: string[];
 
-  copyObject(
-    name?: string,
-    surname?: string,
-    age?: number,
-    favGame?: string,
-    friends?: string[]
-  ) {
-    name != undefined ? this.name == name : (name = this.name);
-    surname != undefined ? this.surname == surname : (surname = this.surname);
-    age != undefined ? this.age == age : (age = this.age);
-    favGame != undefined ? this.favGame == favGame : (favGame = this.favGame);
-    friends != undefined ? this.friends == friends : (friends = this.friends);
-
-    return new UserData(name, surname, age, favGame, friends);
-
-    // without calling an object, we can jsut create a structured clone
-
-    // let copiedObject = structuredClone({
-    //   name,
-    //   surname,
-    //   age,
-    //   favGame,
-    //   friends,
-    // });
-
-    // return copiedObject;
+  copyObject(data: Partial<UserData>) {
+    return new UserData(
+      data.name != undefined ? data.name : this.name,
+      data.surname != undefined ? data.surname : this.surname,
+      data.age != undefined ? data.age : this.age,
+      data.favGame != undefined ? data.favGame : this.favGame,
+      data.friends != undefined ? data.friends : this.friends
+    );
   }
+
+  // copyObject(
+  //   name?: string,
+  //   surname?: string,
+  //   age?: number,
+  //   favGame?: string,
+  //   friends?: string[]
+  // ) {
+  //   name != undefined ? this.name == name : (name = this.name);
+  //   surname != undefined ? this.surname == surname : (surname = this.surname);
+  //   age != undefined ? this.age == age : (age = this.age);
+  //   favGame != undefined ? this.favGame == favGame : (favGame = this.favGame);
+  //   friends != undefined ? this.friends == friends : (friends = this.friends);
+
+  //   return new UserData(name, surname, age, favGame, friends);
+
+  //   // without calling an object, we can jsut create a structured clone
+
+  //   // let copiedObject = structuredClone({
+  //   //   name,
+  //   //   surname,
+  //   //   age,
+  //   //   favGame,
+  //   //   friends,
+  //   // });
+
+  //   // return copiedObject;
+  // }
 }
--- prototype/prototype.ts
+++ prototype/prototype.ts
@@ -11,15 +11,22 @@ let originalObject = new UserData("Dima", "Smaha", 21, "Witcher 3", [
 console.log(originalObject);
 console.log(typeof originalObject);
 
-let copiedObject1 = originalObject.copyObject("Robert", undefined, 31);
-let copiedObject2 = originalObject.copyObject(
-  "Jason",
-  undefined,
-  35,
-  undefined,
-  ["Alexa"]
-);
-let copiedObject3 = originalObject.copyObject(undefined, "Rass");
+// let copiedObject1 = originalObject.copyObject("Robert", undefined, 31);
+// let copiedObject2 = originalObject.copyObject(
+//   "Jason",
+//   undefined,
+//   35,
+//   undefined,
+//   ["Alexa"]
+// );
+// let copiedObject3 = originalObject.copyObject(undefined, "Rass");
+
+let copiedObject1 = originalObject.copyObject({ name: "Robert" });
+let copiedObject2 = originalObject.copyObject({ name: "Leo", age: 33 });
+let copiedObject3 = originalObject.copyObject({
+  name: "Sam",
+  friends: ["Ses"],
+});
 console.log(copiedObject1);
 console.log(copiedObject2);
 console.log(copiedObject3);
